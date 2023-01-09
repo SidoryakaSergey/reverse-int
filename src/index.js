c0fafc0333d540b444b00c8956116c6e565300b6
Thanatus <s.sidoryaka@gmail.com>
2023-01-09 02:06:03 +0200

    feat: reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const arrN =
+        n < 0
+            ? Array.from(String(Math.abs(n)), Number)
+            : Array.from(String(n), Number);
+    return parseInt(arrN.reverse().join(""));
+};
+
+// console.log(reverse(123));
